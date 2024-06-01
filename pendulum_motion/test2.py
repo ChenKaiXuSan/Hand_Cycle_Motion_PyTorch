3de3579fdb6ffead2fc9ad589c6fd26189d7fe12
chen <chenkaixusan@gmail.com>
2024-06-01 16:58:22 +0000

    Multi-threaded generation of pendulums

--- pendulum_motion/test2.py
+++ pendulum_motion/test2.py
@@ -23,27 +23,15 @@ Date      	By	Comments
 
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
-
-# 设置单摆的参数
-g = 9.81  # 重力加速度 (m/s^2)
-L = 1.0   # 单摆长度 (m)
-theta0 = np.pi / 6  # 初始角度 (45度)
-omega0 = 0.0        # 初始角速度
-asymmetry = 0.1  # 不对称系数
-
-# 非对称的运动方程
-left_theta0 = 90
-right_theta0 = 90
-
-# 初始化
-dt = 0.1  # 时间步长
+import cv2, os, json, shutil
+import itertools
+import threading
 
 def one_cycle(max_left_theta, max_right_theta, L, dt):
 
     x_list = []
     y_list = []
-
+    L = 1
     # left to right 
     left_degree = max_left_theta
     while left_degree > 0:
@@ -75,63 +63,70 @@ def one_cycle(max_left_theta, max_right_theta, L, dt):
     
     return x_list, y_list
 
-# 推测一周周期的frame
-total_degree = left_theta0 + right_theta0
-frames = total_degree * 2 / dt 
+def save_index(x_list, file_path):
 
-for _ in range(1):
-    # 一次单摆运动
-    # 按照4个象限来分别的话，从左到右的摆动和从右到左的摆动
-    # 角度的变化是角边小，变大，半个周期结束，再变小，再变大，一个周期结束。
-    x_list, y_list = one_cycle(left_theta0, right_theta0, L, dt)
+    left_index = x_list.index(min(x_list))
+    right_index = x_list.index(max(x_list))
 
-print(f"x_min: {min(x_list)}, x_max: {max(x_list)}")
+    json_info = {
+        "frames": len(x_list),
+        "left_index": left_index,
+        "right_index": right_index
+    }
 
-# 创建动画
-fig, ax = plt.subplots()
-ax.set_xlim(-L-0.1, L+0.1)
-ax.set_ylim(-L-0.1, 0)
-line, = ax.plot([], [], lw=5)
+    with open(file_path, 'w') as f:
+        json.dump(json_info, f, indent=4)
 
-# 这里可以换成其他的形状
-pendulum_bob = plt.Circle((0, 0), 0.1, fc='r')  # 使用圆形表示摆的端点
-pandulum_rect = plt.Rectangle((0, 0), 0.2, 0.1, fc='r')
-polygon = plt.Polygon([[0, 0], [0.1, 0.1], [0.1, -0.1]], closed=True, fill=True, edgecolor='r')
+# def modify_gap(x, y, gap):
+    
+#     # 根据点计算出角度
+#     degree = np.degrees(np.arctan2(y,x))
 
-def init():
+#     # 根据角度来计算新的gap
+#     if x < 0:
+#         x += np.sin(np.radians(degree)) * gap
+#     else:
+#         x -= np.sin(np.radians(degree)) * gap
+
+#     y += np.cos(np.radians(degree)) * gap
+
+#     return x, y
+
+def save_mp4(frames, shape_instance, shape_path, x_list, y_list, L):
+
+    # 创建动画
+    fig, ax = plt.subplots()
+    plt.axis('off')
+    fig.set_size_inches(5.2, 5.2)
+
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 0)
+
+    line, = ax.plot([], [], lw=5)
     line.set_data([], [])
-    pendulum_bob.set_center((0, 0))
-    pandulum_rect.set_xy([0, 0])
-    polygon.set_xy([[0, 0], [0.1, 0.1], [0.1, -0.1]])
 
-    ax.add_patch(pendulum_bob)
-    ax.add_patch(pandulum_rect)
-    ax.add_patch(polygon)
+    ax.add_patch(shape_instance)
 
-    return line, pendulum_bob
-
-def update(frame):
-    x = x_list[frame]
-    y = y_list[frame]
-    line.set_data([0, x], [0, y])
-    pendulum_bob.set_center((x, y))
-    pandulum_rect.set_xy([x-0.1, y-0.1])
-    polygon.set_xy([[x, y], [x+0.1, y+0.1], [x+0.1, y-0.1]])
-    # print(f"frame: {frame}, x: {x}, y: {y}")
-    return line, pendulum_bob
-
-def save_mp4():
-    filename = "asymmetric_pendulum_motion.mp4"
-    fps = 30 
     width, height = fig.get_size_inches() * fig.get_dpi()
-    size = (int(width), int(height))
 
-    init()
-
-    out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
+    out = cv2.VideoWriter(shape_path, cv2.VideoWriter_fourcc(*'mp4v'), 30, (int(width), int(height)))
 
     for i in range(int(frames)):
-        update(i)
+        
+        x = x_list[i]
+        y = y_list[i]
+
+        shape = shape_path.split('_')[-1].split('.')[0]
+        # 根据摆的位置更新图形
+        line.set_data([0, x], [0, y])
+
+        # 感觉这边gap有问题
+        if 'circle' in shape:
+            shape_instance.set_center([x, y])
+        elif 'rect' in shape:
+            shape_instance.set_xy([x, y])
+        elif 'polygon' in shape:
+            shape_instance.set_xy([[0, 0], [x, y], [x+0.5, y+0.5]])            
 
         fig.canvas.draw()
 
@@ -139,6 +134,173 @@ def save_mp4():
         image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
 
         out.write(image)
+
+    out.release()
+    print(f"save {shape_path} done!")
+    # # 复制文件
+    # for i in range(5):
+    #     shutil.copyfile(filename, f"{filename.replace('.mp4', f'_{i}.mp4')}")
+
+def main(shape: str, infos: list):
+
+    # 设置单摆的参数
+    # g = 9.81  # 重力加速度 (m/s^2)
     
-# 保存动画到文件
-save_mp4()
+    dt = 0.1  # 时间步长
+
+    # 非对称的运动方程
+    degree = [90, 45]
+    
+    save_config = []
+
+    for (left_degree, right_degree) in list(itertools.product(degree, degree)):
+
+        print(f"left_degree: {left_degree}, right_degree: {right_degree}")
+
+        # 保存动画到文件
+        path = os.path.join("/workspace/data/pendulum", f"left{left_degree}_right{right_degree}")
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+
+        # 保存index，作为融合的索引
+        index_path = os.path.join("/workspace/data/pendulum/index_mapping", )
+
+        if not os.path.exists(index_path):
+            os.makedirs(index_path, exist_ok=True)
+
+        if shape == 'circle':
+            circle_config = []
+            for idx, info in enumerate(infos):
+                print(f"shape: {shape}, info: {info}")
+                shape_instance = plt.Circle((0, 0), info[0], fc='r')  # 使用圆形表示摆的端点
+                l_gap = info[1]
+
+                # 保存圆形的配置
+                config = {
+                    'left_degree': left_degree,
+                    'right_degree': right_degree,
+                    'radius': info[0],
+                    'l_gap': l_gap
+                }
+
+                circle_config.append(config)
+
+                shape_path = os.path.join(path, f"left{left_degree}_right{right_degree}_{shape}{idx}.mp4")
+                shape_index_path = os.path.join(index_path, f"left{left_degree}_right{right_degree}_{shape}{idx}.json")
+
+                process_one_sample(left_degree, right_degree, l_gap, dt, shape_instance, shape_path, shape_index_path)
+
+        elif shape == 'rect':
+            rect_config = []
+            for idx, info in enumerate(infos):
+
+                width = info[0]
+                height = info[1]
+                shape_instance = plt.Rectangle((0, 0), width, height, fc='r')
+                l_gap = info[2]
+
+                config = {
+                    'left_degree': left_degree,
+                    'right_degree': right_degree,
+                    'width': width,
+                    'height': height,
+                    'l_gap': l_gap
+                }
+                rect_config.append(config)
+
+                shape_path = os.path.join(path, f"left{left_degree}_right{right_degree}_{shape}{idx}.mp4")
+                shape_index_path = os.path.join(index_path, f"left{left_degree}_right{right_degree}_{shape}{idx}.json")
+
+                process_one_sample(left_degree, right_degree, l_gap, dt, shape_instance, shape_path, shape_index_path)
+
+        elif shape == 'polygon':
+            polygon_config = []
+            for idx, info in enumerate(infos):
+
+                one_point = info[0]
+                two_point = info[1]
+                shape_instance = plt.Polygon([[0, 0], [one_point, one_point], [two_point, two_point]], closed=True, fill=True, edgecolor='r')
+                l_gap = info[2]
+
+                config = {
+                    'left_degree': left_degree,
+                    'right_degree': right_degree,
+                    'one_point': one_point,
+                    'two_point': two_point,
+                    'l_gap': l_gap
+                }
+                polygon_config.append(config)
+
+                shape_path = os.path.join(path, f"left{left_degree}_right{right_degree}_{shape}{idx}.mp4")
+                shape_index_path = os.path.join(index_path, f"left{left_degree}_right{right_degree}_{shape}{idx}.json")
+
+                process_one_sample(left_degree, right_degree, l_gap, dt, shape_instance, shape_path, shape_index_path)
+
+    # save config 
+    save_config = {
+        'circle': circle_config,
+        'rect': rect_config,
+        'polygon': polygon_config
+    }
+    with open(os.path.join(path, 'config.json'), 'w') as f:
+        json.dump(save_config, f, indent=4)
+
+def process_one_sample(left_degree, right_degree, L, dt, shape_value, shape_path, shape_index_path):   
+
+    # 一次单摆运动
+    # 按照4个象限来分别的话，从左到右的摆动和从右到左的摆动
+    # 角度的变化是角边小，变大，半个周期结束，再变小，再变大，一个周期结束。
+
+    x_list, y_list = one_cycle(left_degree, right_degree, L, dt)
+
+    # 推测一周周期的frame
+    total_degree = left_degree + right_degree
+    frames = total_degree * 2 / dt 
+    
+    save_mp4(frames, shape_value, shape_path, x_list, y_list, L)
+    save_index(x_list, shape_index_path)
+
+def random_shape():
+
+    # 配置形状，从三个形状里面选择一个
+    # 一个形状里面，大小不同，以及在轴上的大小不同
+    shape = ['circle', 'rect', 'polygon']
+
+    # 随机生成形状，以及配置
+    # circle
+    random_radius = np.linspace(0.1, 0.5, 10)
+    circle_list = list(itertools.product(random_radius, random_radius))
+    
+    # rect
+    rect_list = list(itertools.product(random_radius, random_radius, random_radius))    
+    
+    # polygon
+    polygon_list = list(itertools.product(random_radius, random_radius, random_radius))
+
+    shape_info = {
+        'circle': circle_list,
+        'rect': rect_list,
+        'polygon': polygon_list
+    }
+    
+    return shape_info
+
+if '__main__' == __name__:
+
+    threads = []
+
+    shape_info = random_shape()
+
+    # for shape, shape_info in shape_info.items():
+    #     main(shape, shape_info) # only for test
+
+    for shape, shape_info in shape_info.items():
+
+        thread = threading.Thread(target=main, args=(shape, shape_info))
+        threads.append(thread)
+
+    for t in threads:
+        t.start()
+    
+    for t in threads:
+        t.join()
